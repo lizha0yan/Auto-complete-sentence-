215195559dcfafc973b1fe76495c4b321fdb6b4d
Zhaoyan Li <34900010+lizha0yan@users.noreply.github.com>
2019-08-15 15:37:08 -0700

    Add files via upload

--- Auto Compete Sentence.py
+++ Auto Compete Sentence.py
@@ -13,30 +13,33 @@ from pytorch_transformers import GPT2Tokenizer, GPT2LMHeadModel
 tokenizer = GPT2Tokenizer.from_pretrained('gpt2')
 
 # Encode a text inputs
-text = "I like to watch this movie."
-indexed_tokens = tokenizer.encode(text)
-
-# Convert indexed tokens in a PyTorch tensor
-tokens_tensor = torch.tensor([indexed_tokens])
-
-# Load pre-trained model (weights)
-model = GPT2LMHeadModel.from_pretrained('gpt2')
-
-# Set the model in evaluation mode to deactivate the DropOut modules
-model.eval()
-
-# If you have a GPU, put everything on cuda
-#tokens_tensor = tokens_tensor.to('cuda')
-#model.to('cuda')
-
-# Predict all tokens
-with torch.no_grad():
-    outputs = model(tokens_tensor)
-    predictions = outputs[0]
-
-# Get the predicted next sub-word
-predicted_index = torch.argmax(predictions[0, -1, :]).item()
-predicted_text = tokenizer.decode(indexed_tokens + [predicted_index])
+text = "How to use the API"
+predicted_text = text
+while predicted_text[-1] not in {'.','?','!','~','\n'}:  
+    text = predicted_text
+    indexed_tokens = tokenizer.encode(text)
+    
+    # Convert indexed tokens in a PyTorch tensor
+    tokens_tensor = torch.tensor([indexed_tokens])
+    
+    # Load pre-trained model (weights)
+    model = GPT2LMHeadModel.from_pretrained('gpt2')
+    
+    # Set the model in evaluation mode to deactivate the DropOut modules
+    model.eval()
+    
+    # If you have a GPU, put everything on cuda
+    #tokens_tensor = tokens_tensor.to('cuda')
+    #model.to('cuda')
+    
+    # Predict all tokens
+    with torch.no_grad():
+        outputs = model(tokens_tensor)
+        predictions = outputs[0]
+    
+    # Get the predicted next sub-word
+    predicted_index = torch.argmax(predictions[0, -1, :]).item()
+    predicted_text = tokenizer.decode(indexed_tokens + [predicted_index])
 
 # Print the predicted word
 print(predicted_text)
